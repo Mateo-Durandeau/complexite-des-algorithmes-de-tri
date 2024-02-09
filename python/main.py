f148040f0c10a4bce71d0038370e7f693f9d635e
mateo <110668071+Mateo-Durandeau@users.noreply.github.com>
2024-02-09 21:21:31 +0100

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,6 +1,16 @@
 import utiles
 
-
+# Pour appeler la fonction test_tri_run : 
+#   Args: 
+#         Taille du nombres de nombres
+#         nombre de tableau créer
+#         pas de difference
+#         numéro d'algorithme de tri définis dans le script utile.py
+#         1 = verifier si le tableau est trié 
+#         1 = afficher le tableau de nombres trié  
+#         Choix du cas de test : 1 = cm ; 2 = pc ; 3 = mc
+#         Nom du fichier de save 
+#         nombres d'élément en 1 et x pour le cas moyen
 
 utiles.test_tri_run(10000, 1, 100, 100, 1, 0, 1, "py_resultat_cm_1.csv", 100)
 utiles.test_tri_run(10000, 1, 100, 100, 1, 0, 1, "py_resultat_cm_1.csv", 1000)
