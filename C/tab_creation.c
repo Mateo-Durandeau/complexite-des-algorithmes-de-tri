cd5994d7736fc35aa8e64a1f614e3567c8585f1d
mateo <110668071+Mateo-Durandeau@users.noreply.github.com>
2024-02-09 20:56:54 +0100

    Update and rename tab_creation.c to C/tab_creation.c

--- C/tab_creation.c
+++ C/tab_creation.c
@@ -4,66 +4,66 @@
 #include <time.h>
 
 /*
-    * génération des fichiers csv pour effectuer les tests de tri
+    * gÃ©nÃ©ration des fichiers csv pour effectuer les tests de tri
 */
-// ########### fonction création d'un fichier cas moyen (aléatoire) de n taille #############
+// ########### fonction crÃ©ation d'un fichier cas moyen (alÃ©atoire) de n taille #############
 void generer_nombres_csv_cm(const char *nom_fichier, int nb_element, int limite) {
-    // Initialisation du générateur aléatoire
+    // Initialisation du gÃ©nÃ©rateur alÃ©atoire
     srand((unsigned int)time(NULL));
 
-    // Ouvrir le fichier en mode écriture
+    // Ouvrir le fichier en mode Ã©criture
     FILE *fichier = fopen(nom_fichier, "w");
     if (fichier == NULL) {
         perror("Erreur lors de l'ouverture du fichier");
         exit(1);
     }
     printf("Tirage de %d nombres aleatoires entre 0 et %d\n", nb_element, limite);
-    // Tirage et écriture des nombres dans le fichier
+    // Tirage et Ã©criture des nombres dans le fichier
     for (int i = 0; i < nb_element; i++) {
-        // Tirage d'un nombre aléatoire dans l'intervalle [0,limite[
+        // Tirage d'un nombre alÃ©atoire dans l'intervalle [0,limite[
         int nb = (int)(((float)rand() / RAND_MAX) * limite);
-        // Écrire le nombre dans le fichier CSV
+        // Ã‰crire le nombre dans le fichier CSV
         fprintf(fichier, "%d\n", nb);
     }
     fclose(fichier);
 }
 
-// ########### fonction création d'un fichier meilleur cas (croissant) de n taille #############
+// ########### fonction crÃ©ation d'un fichier meilleur cas (croissant) de n taille #############
 void generer_nombres_csv_mc(const char *nom_fichier, int nb_element) {
-    // Ouvrir le fichier en mode écriture
+    // Ouvrir le fichier en mode Ã©criture
     FILE *fichier = fopen(nom_fichier, "w");
     if (fichier == NULL) {
         perror("Erreur lors de l'ouverture du fichier");
         exit(1);
     }
     printf("Tirage de %d nombres croissant\n", nb_element);
-    // Tirage et écriture des nombres dans le fichier
+    // Tirage et Ã©criture des nombres dans le fichier
     for (int i = 0; i < nb_element; i++) {
         int nb = i;
-        // Écrire le nombre dans le fichier CSV
+        // Ã‰crire le nombre dans le fichier CSV
         fprintf(fichier, "%d\n", nb);
     }
     fclose(fichier);
 }
 
-// ########### fonction création d'un fichier pire cas (décroissant) de n taille #############
+// ########### fonction crÃ©ation d'un fichier pire cas (dÃ©croissant) de n taille #############
 void generer_nombres_csv_pc(const char *nom_fichier, int nb_element) {
-    // Ouvrir le fichier en mode écriture
+    // Ouvrir le fichier en mode Ã©criture
     FILE *fichier = fopen(nom_fichier, "w");
     if (fichier == NULL) {
         perror("Erreur lors de l'ouverture du fichier");
         exit(1);
     }
     printf("Tirage de %d nombres decroissants\n", nb_element);
-    // Tirage et écriture des nombres dans le fichier
+    // Tirage et Ã©criture des nombres dans le fichier
     for (int i = nb_element; i >= 1; i--) {
-        // Écrire le nombre dans le fichier CSV
+        // Ã‰crire le nombre dans le fichier CSV
         fprintf(fichier, "%d\n", i);
     }
     fclose(fichier);
 }
 
-// ########### fonction création d'une run de fichier de n taille et au choix du cas #############
+// ########### fonction crÃ©ation d'une run de fichier de n taille et au choix du cas #############
 void generation_run(int taille_fich, int choix_cas, int Nb_tab, int pas_difference, int limite) {
     // choix cas : 1 = cas moyen (hasard)
                 // 2 = pire cas
@@ -75,7 +75,7 @@ void generation_run(int taille_fich, int choix_cas, int Nb_tab, int pas_differen
 
     for (int i = 1; i <= Nb_tab; i++) {
         // Construire le nom de fichier en utilisant snprintf
-        char nom_fichier[40];  // Ajustez la taille en conséquence
+        char nom_fichier[40];  // Ajustez la taille en consÃ©quence
         if (choix_cas == 1)
             snprintf(nom_fichier, sizeof(nom_fichier), "tableau/cm/tab_cm_%d_%d_%03d", limite, taille_fich, i);
         if (choix_cas == 2)
@@ -85,7 +85,7 @@ void generation_run(int taille_fich, int choix_cas, int Nb_tab, int pas_differen
         // Ajouter l'extension .csv au nom du fichier
         strcat(nom_fichier, ".csv");
 
-        // Appeler la fonction pour générer le fichier CSV
+        // Appeler la fonction pour gÃ©nÃ©rer le fichier CSV
         if (choix_cas == 1)
             generer_nombres_csv_cm(nom_fichier, taille_fich, limite);
         if (choix_cas == 2)
@@ -94,7 +94,7 @@ void generation_run(int taille_fich, int choix_cas, int Nb_tab, int pas_differen
             generer_nombres_csv_mc(nom_fichier, taille_fich);
 
         taille_fich = taille_fich - pas_difference;
-        // affichage du nombre restant de fichier à générer
+        // affichage du nombre restant de fichier Ã  gÃ©nÃ©rer
         printf("%d / %d fini", i, Nb_tab);
     }
 }
